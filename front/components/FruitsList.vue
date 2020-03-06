fe4f68ab3901f59a8b30b748930a3d73fd29df66
apu314 <apu314@apu314.com>
2020-03-06 19:45:48 +0000

    - CRUD Fruits (List, Create, Read, Update, Destroy)

---
+++ front/components/FruitsList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <ul>
+      <li class="list--item" v-for="fruit in fruits">
+        <div>{{ fruit.name }}</div>
+        <div>{{ fruit.fruit_details[0].size }}</div>
+        <div>{{ fruit.fruit_details[0].color }}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        fruits: ''
+      }
+    },
+    created () {
+      this.$axios.get('fruits')
+        .then(response => {
+          this.fruits = response.data
+        })
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
+
+
